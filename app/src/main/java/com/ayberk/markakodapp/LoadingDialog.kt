b9ec968c558ee1a6b79e1d8307beb20b368b365f
ayberktmn <ayberktemin@gmail.com>
2023-07-13 12:41:24 +0300

    Fragment animation

---
+++ app/src/main/java/com/ayberk/markakodapp/LoadingDialog.kt
@@ -0,0 +1,27 @@
+package com.ayberk.markakodapp
+
+import android.app.AlertDialog
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import com.ayberk.markakodapp.databinding.LoadingItemBinding
+
+class LoadingDialog(private val myFragment: Fragment) {
+    private var dialog: AlertDialog? = null
+
+    fun startLoading() {
+        val inflater = LayoutInflater.from(myFragment.requireContext())
+        val binding = LoadingItemBinding.inflate(inflater)
+
+        val builder = AlertDialog.Builder(myFragment.requireContext())
+        builder.setView(binding.root)
+        builder.setCancelable(false)
+        dialog = builder.create()
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog?.show()
+        binding.animationView.speed = 2f
+    }
+
+    fun dismiss() {
+        dialog?.dismiss()
+    }
+}
